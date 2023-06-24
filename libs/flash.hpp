519dbb8b9325f38c523599a1daedc141c984c595
rafaelBauer <abauer.rafael@gmail.com>
2023-06-24 12:31:33 +0200

    Flash working

--- libs/flash.hpp
+++ libs/flash.hpp
@@ -1,14 +1,14 @@
 /*******************************************************************************
-* @file                    flash.hpp
-* @author                  Rafael Andrioli Bauer
-* @date                    22.06.2023
-* @matriculation number    5163344
-* @e-mail contact          abauer.rafael@gmail.com
-*
-* @brief   Header file with the prototypes of the Flash C functions as well as the
-*          declaration of the Flash class.
-*          Its implementation must be in the header because it is templated.
-******************************************************************************/
+ * @file                    flash.hpp
+ * @author                  Rafael Andrioli Bauer
+ * @date                    22.06.2023
+ * @matriculation number    5163344
+ * @e-mail contact          abauer.rafael@gmail.com
+ *
+ * @brief   Header file with the prototypes of the Flash C functions as well as the
+ *          declaration of the Flash class.
+ *          Its implementation must be in the header because it is templated.
+ ******************************************************************************/
 
 #ifndef LIBS_FLASH_H_
 #define LIBS_FLASH_H_
@@ -18,6 +18,9 @@ namespace AdvancedMicrotech {
 
 template<typename CS, typename HOLD, typename WP, typename SPI>
 class FLASH_T {
+  static constexpr uint8_t M25P16_MANUFACTURER = 0x20;
+  static constexpr uint8_t M25P16_MEMORY_TYPE = 0x20;
+
 public:
   /**
    * Initialize the flash chip
@@ -29,13 +32,11 @@ public:
     SPI::init();
     uint8_t ID[3]{0};
 
-    CS::set_low();
-    uint8_t instruction = 0x9F;
-    SPI::write(1, &instruction);
-    SPI::read(3, &ID[0]);
+    sendInstruction(READ_ID, false);
+    SPI::read(sizeof(ID), &ID[0]);
     CS::set_high();
 
-    if (ID[0] != 0x20) {
+    if (ID[0] != M25P16_MANUFACTURER || ID[1] != M25P16_MEMORY_TYPE) {
       while (1) {
         __no_operation();
       }
@@ -53,6 +54,7 @@ public:
   static constexpr void write(uint32_t address, uint16_t length, uint8_t* data) {
     waitUntilNotBusy();
     sendInstructionToAddress(address, SECTOR_ERASE, true);
+    waitUntilNotBusy();
     sendInstruction(WRITE_ENABLE, true);
     waitUntilNotBusy();
     sendInstructionToAddress(address, PAGE_PROGRAM, false);
@@ -72,34 +74,34 @@ private:
     sendInstruction(READ_STATUS_REGISTER, false);
     SPI::read(sizeof(status), &status);
     CS::set_high();
-    //StatusRegister retVal;
-    //retVal.busy = status & BUSY_MASK;
-    //retVal.writeEnabled = status & WRITE_ENABLED_MASK;
+    // StatusRegister retVal;
+    // retVal.busy = status & BUSY_MASK;
+    // retVal.writeEnabled = status & WRITE_ENABLED_MASK;
     return {status & BUSY_MASK, status & WRITE_ENABLED_MASK};
   }
-
 
   static constexpr void sendInstruction(uint8_t instruction, bool finalMessage) {
     CS::set_low();
     SPI::write(1, &instruction);
-    if(finalMessage) {
+    if (finalMessage) {
       CS::set_high();
     }
   }
 
   static constexpr void sendInstructionToAddress(uint32_t address, uint8_t instruction, bool finalMessage) {
-      uint8_t instructionAndAddress[4] = {instruction, static_cast<uint8_t>((address & 0xFF0000) >> 16), static_cast<uint8_t>((address & 0x00FF00) >> 8),
-                                         static_cast<uint8_t>((address & 0xFF))};
-     CS::set_low();
-     SPI::write(sizeof(instructionAndAddress), &instructionAndAddress[0]);
-     if(finalMessage) {
-         CS::set_high();
-     }
+    uint8_t instructionAndAddress[4] = {instruction, static_cast<uint8_t>((address & 0xFF0000) >> 16),
+                                        static_cast<uint8_t>((address & 0x00FF00) >> 8),
+                                        static_cast<uint8_t>((address & 0xFF))};
+    CS::set_low();
+    SPI::write(sizeof(instructionAndAddress), &instructionAndAddress[0]);
+    if (finalMessage) {
+      CS::set_high();
+    }
   }
 
   static constexpr void waitUntilNotBusy() {
     StatusRegister statusRegister = readStatusRegister();
-    while(statusRegister.busy) {
+    while (statusRegister.busy) {
       statusRegister = readStatusRegister();
     }
   }
@@ -115,12 +117,12 @@ private:
   static constexpr uint8_t BULK_ERASE{0xC7};
 };
 
-}
+}  // namespace AdvancedMicrotech
 // Read <length> bytes into <rxData> starting from address <address> (1 pt.)
-void flash_read(long int address, unsigned char length, unsigned char * rxData);
+void flash_read(long int address, unsigned char length, unsigned char* rxData);
 
 // Write <length> bytes from <txData>, starting at address <address> (1 pt.)
-void flash_write(long int address, unsigned char length, unsigned char * txData);
+void flash_write(long int address, unsigned char length, unsigned char* txData);
 
 // Returns 1 if the FLASH is busy or 0 if not.
 // Note: this is optional. You will probably need this, but you don't have to
